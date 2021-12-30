2300bed92e48201b6b3f7110cd2b594d7c5f7954
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2021-12-30 20:58:59 +0300

    Update Flatten.py

--- Flatten.py
+++ Flatten.py
@@ -1,1 +1,19 @@
 
+input = eval(input("add a input :"))
+output = []
+
+def isSingle(t):
+    if type(t) ==  str  or type(t) == float or type(t) == int:
+        return True
+    else:
+        return False
+
+def flatten(x):
+    if isSingle(x):
+        output.append(x)
+    else:
+        for i in x:
+            flatten(i)
+
+flatten(input)
+print(output)
