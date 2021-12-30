ca6f17504f8516fdf4d3dfc983ca74c701448e78
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2021-12-30 21:00:38 +0300

    Update Reverse.py

--- Reverse.py
+++ Reverse.py
@@ -1,1 +1,21 @@
 
+input = eval(input("add a input :"))
+
+def isList(t):
+    if type(t) ==  list:
+        return True
+    else:
+        return False
+
+def reverse(x):
+    if isList(x):
+        for i,e in enumerate(x):
+            if isList(e):
+                x[i] = e[::-1]
+        x = x[::-1]
+    return x
+    
+
+
+print(reverse(input))
+
